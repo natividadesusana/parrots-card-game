5ec0381cbc03ebf81c541ae820fa4d2a4223880a
natividadesusana <susanajdsn@gmail.com>
2023-01-12 03:20:05 -0300

    finalizing card distribution

--- js/script.js
+++ js/script.js
@@ -1,31 +1,50 @@
 const cards = [];
+
 const gifts = [
-    './img/discord.gif',
-    './img/pooh.gif'
+    `<img src="/img/christmas.gif">`,
+    `<img src="/img/discord.gif">`,
+    `<img src="/img/ghibli.gif">`,
+    `<img src="/img/pooh.gif">`,
+    `<img src="/img/spinner.gif">`,
+    `<img src="/img/sticker.gif">`,
 ];
+
 let counter = 0;
-let amountGifts = null;
+
 const listLetters = document.querySelector('.card');
 
 function startGame() {
 
-    let amountLetters = parseInt(prompt(`Com quantas cartas deseja jogar?
-Obs: Digite um número par entre 4 e 14!`));
+    let amountLetters = parseInt(prompt(`🔹 Com quantas cartas deseja jogar?
+    🔺 Obs: Digite um número par entre 4 e 14!`));
 
     while ((amountLetters % 2 !== 0) || (amountLetters < 4) || (amountLetters > 14)) {
-        amountLetters = parseInt(prompt(`⚠️ Atenção!!
-Digite um número par entre 4 e 14!`));
+        amountLetters = parseInt(prompt(`🔺 Atenção!!
+    ➡️ Digite um número par entre 4 e 14!`));
     };
 
     for (let i = 0; i < (amountLetters / 2); i++) {
-        cards.push(i);
+        cards.push(gifts[i], gifts[i]);
     };
 
-    amountGifts = amountLetters / 2;
-    cards.sort(random);
+    gifts.sort(randomL);
+    cards.sort(randomL);
+
+    for (let i = 0; i < amountLetters; i++) {
+        const box = document.querySelector('.box-cards');
+        box.innerHTML += `
+        <div class="card" onclick="turn(this)">
+            <div class="front-face face">
+                <img src="img/back.png">
+            </div>
+            <div class="back-face face">
+                ${cards[i]}
+            </div>
+        </div>`
+    }
 }
 
-function random() {
+function randomL() {
     return Math.random() - 0.5;
 }
 
